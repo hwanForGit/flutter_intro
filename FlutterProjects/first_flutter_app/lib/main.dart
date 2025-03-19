62f4ff6f48c2c400de1972b6071f346facabdfb6
gitHwan <moongodd88@gmail.com>
2025-03-20 01:31:50 +0900

    0320_flutter_commit

--- FlutterProjects/first_flutter_app/lib/main.dart
+++ FlutterProjects/first_flutter_app/lib/main.dart
@@ -33,18 +33,34 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // 변수
+  int count = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('홈')),
       body: Center(
-        child: Text(
-          '0',
-          style: TextStyle(
-            color: Colors.red,
-            fontSize: 70,
-          ),
-        ),
+        child: Column(
+          children: [
+            Text(
+              '$count', // flutter 에서는 작은 따옴표에 '${변수명}' 으로 표시
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 70,
+              ),
+            ),
+          ],
+        ), // option + enter 키로 wrapping 가능
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // 화면 갱신 - setState() 를 해줘야 화면에 다시 그려준다.
+          setState(() {
+            count++;
+          });
+        },   // onPressed 중괄호 내에 함수 입력
+        child: Icon(Icons.add),
       ),
     );
   }
